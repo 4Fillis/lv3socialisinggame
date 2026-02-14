574f1d1bc371502c6d7fbb1ff07226d7e99f7a58
illiotes <165102269+4Fillis@users.noreply.github.com>
2026-02-15 02:28:01 +1300

    save(start)

---
+++ main.py
@@ -0,0 +1,21 @@
+#2026 game rpg based on tower farm simulator, dnd roles inclusive
+#pc = player characer, user
+
+#main colors for gui
+txt_clr = "#02111b" #main text highlight color
+txtbg_bar_clr = "#d5f1e6" #input bar bg
+txt_bar_clr = "#084446" #user input text color
+bg_clr = "#3899C0" #main text color
+btn_press_bg = "#2D76B1"
+btn_txt_clr = "#143C3F"
+font = "OpenDyslexic"
+fontsize = 12
+
+al = {
+    0:  {"area": "example",
+         'desc': "description\n"},
+    0:  {"area": "example2",
+         'desc': "description2\n"},
+    0:  {"area": "example3",
+         'desc': "description3\n"},}
+
